51ed7a7f8138f810822fb24bb9963fae458c4cd9
VitaliiBaryliuk <vitaliyua912@gmail.com>
2018-11-15 21:44:15 +0200

    js file corrected with eslint

--- dist/js/index2.js
+++ dist/js/index2.js
@@ -1,6 +1,7 @@
 (function () {
   const form = document.querySelector('.my-form');
   const inputs = form.querySelectorAll('.input');
+  const inputsArr = Object.keys(inputs);
   const rules = {
     text: {
       regExp: /['"]/,
@@ -23,57 +24,51 @@
       message: 'Shoud be checked',
     },
   };
-
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    for (input in inputs) {
-      const elem = inputs[input];
-      const inputValue =  elem.value;
-      const dataValue = elem.dataset.value;
-      const div = document.createElement('div');
-      div.classList.add('message-block');
-      elem.closest('label').appendChild(div);
-
-      if (elem.value == '' && elem.name != "sex" ){
-        removeMessage(elem);
-        emptyCorrect(elem, div, 'Shouldn\'t be an empty', 'invalid');
-      } 
-      else if(elem.hasAttribute("data-value") && elem.name != "sex"){
-        if(dataValue == "text" && rules[dataValue].regExp.test(inputValue) == true){
-            errorMessage(elem);
-            removeMessage(elem);
-          }
-        else if(dataValue == "bool" && elem.checked == false){
-            errorMessage(elem);
-            removeMessage(elem);
-        }
-        else if(rules[dataValue].regExp.test(inputValue) == false && dataValue !== "text"){
-            errorMessage(elem);
-            removeMessage(elem);
-        }
-        else{
-          emptyCorrect(elem, div, 'Correct!', 'valid')
-          removeMessage(elem);
-        }
-      }
-    }
-  });
-
-  function emptyCorrect(inp, div, mes, valClass){
+  function emptyCorrect(inp, div, mes, valClass) {
     inp.classList.remove('invalid');
     inp.classList.add(valClass);
     div.innerHTML = mes;
   }
 
-  function errorMessage(inp){
-    inp.classList.remove("invalid");
+  function errorMessage(inp, div, dataValue) {
+    inp.classList.add('invalid');
     div.innerHTML = rules[dataValue].message;
-  } 
-
-  function removeMessage(inp){
-    if(inp.closest('.parent').childNodes.length > 1){
+  }
+  
+  function removeMessage(inp) {
+    if (inp.closest('.parent').childNodes.length > 1) {
       inp.closest('.parent').childNodes[2].remove();
     }
   }
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    for (let i = 0; i < inputsArr.length; i += 1) {
+      const elem = inputs[i];
+      const inputValue = elem.value;
+      const dataValue = elem.dataset.value;
+      const div = document.createElement('div');
+      div.classList.add('message-block');
+      elem.closest('label').appendChild(div);
+
+      if (elem.value === '' && elem.name !== 'sex') {
+        removeMessage(elem);
+        emptyCorrect(elem, div, 'Shouldn\'t be an empty', 'invalid');
+      } else if (elem.hasAttribute('data-value') && elem.name !== 'sex') {
+        if (dataValue === 'text' && rules[dataValue].regExp.test(inputValue) === true) {
+          errorMessage(elem, div, dataValue);
+          removeMessage(elem);
+        } else if (dataValue === 'bool' && elem.checked === false) {
+          errorMessage(elem, div, dataValue);
+          removeMessage(elem);
+        } else if (rules[dataValue].regExp.test(inputValue) === false && dataValue !== 'text') {
+          errorMessage(elem, div, dataValue);
+          removeMessage(elem);
+        } else {
+          emptyCorrect(elem, div, 'Correct!', 'valid');
+          removeMessage(elem);
+        }
+      }
+    }
+  });
 }());
